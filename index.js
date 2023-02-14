f88121ab530159af8b2af5f5e0529623ee508c6d
lucasescobosa <lucasescobosa@gmail.com>
2023-02-14 20:56:34 -0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ("express")
 const morgan = require ('morgan') //logger
-//const cors = require ('cors')
+const cors = require ('cors')
 const methodOverride =  require('method-override'); // Para poder usar los métodos PUT y DELETE
 const db = require('./database/models')
 
@@ -12,10 +12,13 @@ process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 
 app.use(express.json())
 app.use(methodOverride('_method'));
-/*app.use(cors({
-    origin: ['http://localhost:3000', 'https://vercel-deploy-rosy-beta.vercel.app/'],
-    methods: 'GET, PUT, POST, DELETE'
-}))*/
+app.use(cors())
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept, Authorization")
+    next()
+})
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
