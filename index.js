ed493fdad1748e4845fc13bad416a3470a69177c
lucasescobosa <lucasescobosa@gmail.com>
2023-02-27 16:52:49 -0300

    test

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 
 app.use(express.json())
 app.use(methodOverride('_method'));
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://vercel-deploy-rosy-beta.vercel.app/'],
-    methods: 'GET, PUT, POST, DELETE'
-}))
+app.use(cors())
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept, Authorization")
+    next()
+})
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
